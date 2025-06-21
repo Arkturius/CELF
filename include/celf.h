5ec1edf5ccf776e097e4895eb3a80108f21358ef
Arkturius <63616260+Arkturius@users.noreply.github.com>
2025-06-21 13:30:42 +0200

    Static library linking

--- include/celf.h
+++ include/celf.h
@@ -419,7 +419,7 @@ CELF_API(int, ELF_is32bit);
     type32 CONCAT(ELF32_, name) args32;										\
     type64 CONCAT(ELF64_, name) args64;										\
 																			\
-    ret CONCAT(ELF_, name) wrap_args										\
+    INLINE ret CONCAT(ELF_, name) wrap_args									\
     {																		\
         if (ELF_is32bit())													\
             return CONCAT(ELF32_, name) call32;								\
